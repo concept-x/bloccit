a55613d05afc09f545cd43ce3748157e595f0024
concept-x <emilie_elliotthotmail.com>
2018-03-11 18:02:57 -0500

    cp-12-rails-testing: complete

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+# #1 describe subject of spec, WelcomeController
+RSpec.describe WelcomeController, type: :controller do
+    describe "GET index" do
+        it "renders the index template" do
+    # #2 use 'get' to call 'index' method of WC
+            get :index
+    # #3 expect WC's reponse to render index template
+            expect(response).to render_template("index")
+        end
+    end
+    
+    describe "GET about" do
+        it "renders the about template" do
+            get :about
+            expect(response).to render_template("about")
+        end
+    end
+end
